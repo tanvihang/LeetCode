a76293b39712db20c8f8fc6f9ac88896e6f7893a
tanvihang <tvhang7@gmail.com>
2023-12-04 15:10:25 +0800

    Update Folder Structure

--- C#/Tree/1038BinarySearchTreetoGreaterSumTree/Program.cs
+++ C#/Tree/1038BinarySearchTreetoGreaterSumTree/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("AHAHAHA");
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        return temperatures;
+    }
+}
+
+public class Program{
+    public static void Main(){
+        Console.WriteLine("Main");
+
+        Solution mySolution = new Solution();
+
+        int [] arr = new int[2];
+        arr[0] = 2;
+        arr[1] = 1;
+
+        int[] ans = mySolution.DailyTemperatures(arr);
+        System.Console.WriteLine(ans[0]);
+    }
+}
