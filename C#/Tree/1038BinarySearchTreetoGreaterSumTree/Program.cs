542f9d51bc28331392e363699d3aa9079bc4a8a1
tanvihang <tvhang7@gmail.com>
2023-12-04 20:40:30 +0800

    update gitignore

--- C#/Tree/1038BinarySearchTreetoGreaterSumTree/Program.cs
+++ C#/Tree/1038BinarySearchTreetoGreaterSumTree/Program.cs
@@ -1,22 +1,58 @@
-﻿using System.Reflection;
+﻿namespace System;
 
+//   Definition for a binary tree node.
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+ 
 public class Solution {
-    public int[] DailyTemperatures(int[] temperatures) {
-        return temperatures;
+    public TreeNode BstToGst(TreeNode root) {
+        TreeNode tmp = root;
+        int count = 0;
+        InOrderTraversal(tmp, ref count);
+        return root;
+    }
+
+    public void InOrderTraversal(TreeNode node, ref int count){
+        if(node == null){
+            return;
+        }
+
+        InOrderTraversal(node.right,ref count);
+        node.val += count;
+        count = node.val;
+        InOrderTraversal(node.left,ref count);
+
+
     }
 }
 
 public class Program{
     public static void Main(){
-        Console.WriteLine("Main");
+        TreeNode root = new TreeNode(4,new TreeNode(1,new TreeNode(0,null,null),new TreeNode(2,null,new TreeNode(3,null,null))),new TreeNode(6,new TreeNode(5,null,null),new TreeNode(7,null,new TreeNode(8,null,null))));
 
-        Solution mySolution = new Solution();
+        Solution solution = new Solution();
+        solution.BstToGst(root);
+        PrintNodeInOrder(root);
+    
+    }
 
-        int [] arr = new int[2];
-        arr[0] = 2;
-        arr[1] = 1;
+     public static void PrintNodeInOrder(TreeNode node){
+        if(node == null){
+            return;
+        }
 
-        int[] ans = mySolution.DailyTemperatures(arr);
-        System.Console.WriteLine(ans[0]);
+        PrintNodeInOrder(node.right);
+        Console.WriteLine(node.val);
+        PrintNodeInOrder(node.left);
+        
     }
 }
